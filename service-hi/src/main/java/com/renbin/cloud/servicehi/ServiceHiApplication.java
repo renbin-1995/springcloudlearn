2720538b51de311d50143026a4a2fd6ef5203b88
renbin <bin.ren@advance.ai>
2019-10-12 20:17:32 +0800

    Spring cloud 链路追踪

--- service-hi/src/main/java/com/renbin/cloud/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/renbin/cloud/servicehi/ServiceHiApplication.java
@@ -1,12 +1,18 @@
 package com.renbin.cloud.servicehi;
 
+import brave.sampler.Sampler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @EnableEurekaClient
@@ -17,12 +23,34 @@ public class ServiceHiApplication {
         SpringApplication.run(ServiceHiApplication.class, args);
     }
 
+    private static final Logger logger = LoggerFactory.getLogger(ServiceHiApplication.class.getName());
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hi")
-    public String home(@RequestParam(value = "name", defaultValue = "renBinCloud") String name) {
-        return "hi " + name + " ,i am from port:" + port;
+    public String callHome(@RequestParam(value = "name", defaultValue = "renBinCloud") String name) {
+        logger.info("calling trace service-hi ");
+        logger.info("hi " + name + " ,i am from port:" + port);
+        return restTemplate.getForObject("http://localhost:8881/miya", String.class);
     }
 
+    @RequestMapping("/info")
+    public String info() {
+        logger.info("calling trace service-hi ");
+        return "i'm service-hi";
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
