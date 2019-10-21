cc130df40314013a3c9a3dd6fc4d17b69e5a809b
renbin <bin.ren@advance.ai>
2019-10-21 11:56:03 +0800

    Spring cloud 高可用注册中心

--- service-hi/src/main/java/com/renbin/cloud/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/renbin/cloud/servicehi/ServiceHiApplication.java
@@ -1,19 +1,26 @@
 package com.renbin.cloud.servicehi;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 @RestController
 @EnableEurekaClient
 @SpringBootApplication
@@ -37,10 +44,16 @@ public class ServiceHiApplication {
     String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String callHome(@RequestParam(value = "name", defaultValue = "renBinCloud") String name) {
         logger.info("calling trace service-hi ");
         logger.info("hi " + name + " ,i am from port:" + port);
-        return restTemplate.getForObject("http://localhost:8881/miya", String.class);
+        return "hi " + name + " ,i am from port:" + port;
+        // return restTemplate.getForObject("http://localhost:8881/miya", String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi, " + name + ", sorry, erroe!";
     }
 
     @RequestMapping("/info")
